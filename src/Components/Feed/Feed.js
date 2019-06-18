45b3503f68b7f8c16328b67bd56487d14f163acd
dilrong <dilrong@dilrong.com>
2019-06-18 14:01:20 +0900

    update scrap add localStorage remove

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,19 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Feed.scss';
 import {Profile} from '../index';
 
 
 function Feed({data}) {
     const [src, setSrc] = useState("./images/scrap_off.svg");
+
+    useEffect(() => {
+        let localObj = localStorage.getItem("scrap") ? JSON.parse(localStorage.getItem("scrap")) : [];
+        localObj.map((obj, index) => (
+            (obj.id === data.id) ? setSrc("./images/scrap_on.svg") : setSrc("./images/scrap_off.svg")
+        ))
+    });
+
     return(
         <div className="Feed">
             <Profile nickName={data.nickname} profileImage={data.profile_image_url}/>
             <img className="Rectangle" alt="FeedPhoto" src={data.image_url}/>
             <img className="scrap" alt="scrap" src={src} onClick={()=>{
-                setSrc("./images/scrap_on.svg");
                 let localObj = localStorage.getItem("scrap") ? JSON.parse(localStorage.getItem("scrap")) : [];
-                localObj.push(data);
-                localStorage.setItem("scrap", JSON.stringify(localObj));
+                setSrc("./images/scrap_on.svg");
+                if(src === "./images/scrap_on.svg"){
+                    setSrc("./images/scrap_off.svg");
+                    let index = localObj.findIndex(obj => obj.id === data.id);
+                    localObj.splice(index, 1);
+                    localStorage.setItem("scrap", JSON.stringify(localObj));
+                }else{
+                    localObj.push(data);
+                    localStorage.setItem("scrap", JSON.stringify(localObj));
+                }
                 }}/>
         </div>
     );
